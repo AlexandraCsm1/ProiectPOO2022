9222a2169fa39fa9afa60cf0786c26da56681cbd
AlexandraCsm1 <94761873+AlexandraCsm1@users.noreply.github.com>
2022-03-16 23:34:03 +0200

    Add files via upload

---
+++ ProiectPOO/alegere.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "Header.h"
+using namespace std;
+void alegere() {
+	cout << "Alegeti optiunea dorita: \n";
+	cout << "1. Incarcare biblioteca dintr-un fisier. \n";
+	cout << "2. Creare domeniu nou. \n";
+	cout << "3. Adaugare carte la un anumit domeniu. \n";
+	cout << "4. Cautare carte in biblioteca. \n";
+	cout << "5. Stergere carte din biblioteca. \n";
+	cout << "6. Imprumutare / Returnare carte. \n";
+	cout << "7. Verificare carti cu termen depasit de imprumut. \n";
+	cout << "8. Salvare in fisier a bibliotecii. \n";
+	cout << "9. Iesire. \n";
+
+
+
+}
---
+++ ProiectPOO/alegere.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef alegere_H
+#define alegere_H
+void alegere();
+#endif
